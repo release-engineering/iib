79805bf9ddbd23ab2599af7a64a02b30eea81057
Yashvardhan Nanavati <yashn@bu.edu>
2020-02-06 01:24:45 +0530

    Add Flask boilerplate

---
+++ iib/web/config.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+class Config(object):
+    """The base IIB Flask configuration."""
+    # Additional loggers to set to the level defined in IIB_LOG_LEVEL
+    IIB_ADDITIONAL_LOGGERS = []
+    # This sets the level of the "flask.app" logger, which is accessed from current_app.logger
+    IIB_LOG_LEVEL = 'INFO'
+
+
+class ProductionConfig(Config):
+    """The production IIB Flask configuration."""
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    """The development IIB Flask configuration."""
+    IIB_LOG_LEVEL = 'DEBUG'
+
+
+class TestingConfig(DevelopmentConfig):
+    """The testing IIB Flask configuration."""
+    DEBUG = True
