59fe4d5a86974bedc8ef245bb4d7768829f40250
mprahl <mprahl@users.noreply.github.com>
2020-02-17 13:16:45 -0500

    Pin the dependencies using pip-tools
    Since this will be deployed on RHEL 8 and several of the required
    dependencies are not packaged as RPMs, let's use pip-tools to
    ensure the installed dependencies are deterministic (pinned versions)
    and relatively secure (hashes).

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # iib
 A REST API to manage operator index container images
+
+## Dependency Management
+
+To manage dependencies, this project uses [pip-tools](https://github.com/jazzband/pip-tools) so that
+the production dependencies are pinned and the hashes of the dependencies are verified during
+installation.
+
+The unpinned dependencies are recorded in `setup.py`, and to generate the `requirements.txt` file,
+run `pip-compile --generate-hashes --output-file=requirements.txt`. This is only necessary when
+adding a new package. To upgrade a package, use the `-P` argument of the `pip-compile` command.
+
+To update `requirements-test.txt`, run
+`pip-compile --generate-hashes setup.py requirements-test.in -o requirements-test.txt`.
+
+When installing the dependencies in a production environment, run
+`pip install --require-hashes -r requirements.txt`. Alternatively, you may use
+`pip-sync requirements.txt`, which will make sure your virtualenv only has the packages listed in
+`requirements.txt`.
--- setup.py
+++ setup.py
@@ -1,27 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 from setuptools import setup, find_packages
 
-
-def get_requirements(req_file):
-    """
-    Get the requirements listed in a requirements file.
-
-    :param str req_file: the path to the requirements file, relative to this file
-    :return: the list of requirements
-    :rtype: list
-    """
-    with open(req_file) as fd:
-        lines = fd.readlines()
-
-    dependencies = []
-    for line in lines:
-        dep = line.strip()
-        # Skip comments and inclusion of other requirements files
-        if not dep.startswith('#') and not dep.startswith('-r'):
-            dependencies.append(dep)
-    return dependencies
-
-
 setup(
     name='iib',
     version='1.0',
@@ -29,7 +8,16 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=get_requirements('requirements.txt'),
+    install_requires=[
+        'celery',
+        'flask',
+        'flask-login',
+        'flask-migrate',
+        'flask-sqlalchemy',
+        'psycopg2-binary',
+        'requests',
+        'requests-kerberos',
+    ],
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3.5',
