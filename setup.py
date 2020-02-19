130780acb982b7b170307760b42fe47de96f914f
mprahl <mprahl@users.noreply.github.com>
2020-02-19 09:50:48 -0500

    Skip packaging the tests package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='iib',
     version='1.0',
     long_description=__doc__,
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
