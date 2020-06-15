e56d98931f1144fcd56200c362c6f2fe91d281b3
mprahl <mprahl@users.noreply.github.com>
2020-06-15 15:32:14 -0400

    Release v3.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iib',
-    version='2.8.0',
+    version='3.0.0',
     long_description=__doc__,
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
