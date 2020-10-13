cd2b80e343fc7b2404775fc770b7a6ba3e6567e0
Yashvardhan Nanavati <yashn@bu.edu>
2020-10-13 17:46:46 -0400

    Release v3.9.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iib',
-    version='3.8.2',
+    version='3.9.0',
     long_description=__doc__,
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
