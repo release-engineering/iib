f46c96a1cbaa77136212b65ce4507e0dcba49275
Jonathan Gangi <jgangi@redhat.com>
2021-11-19 11:06:40 -0300

    Upgrade Flask, Werkzeug, Celery, Kombu and Python
        - Upgrade Flask and Werkzeug to 2.0.2 for py36
            - Rewrite the tests "test_json_to_envelope" and
    	  "test_get_ssl_domain_cert_config_not_set! to use the "app" fixture
    
        - Drop support for py37
    
        - Include support for py38
            - Use Separate requirements files since py36 has incompatible dependencies with py38
            - Keep the default requirements.txt for py36
            - Use requirements-py38.txt for py38
            - Allow future deprecation of py36 by just renaming back the requirements files
            - Update tox.ini to support separate envs for tests
    
        - Use py38 as default on Dockerfile-workers and Dockerfile-api
    
        - Upgrade Celery and Kombu
            - On py38 upgrade to celery==5.2.1 and kombu==5.2.2 due to
    	  security issues on < 5.2
            - On py36 upgrade to celery==5.1.2 and kombu==5.1.0 since the
    	  support for py36 was dropped on 5.2
    
    Refers to CLOUDDST-10176

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     entry_points={'console_scripts': ['iib=iib.web.manage:cli']},
     license="GPLv3+",
