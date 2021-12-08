4e15d6a40eea9c891053850c24bcf5e2eb9e4948
Jan Lipovský <jlipovsk@redhat.com>
2021-12-08 15:09:59 +0100

    Fixes for failing flake8 test

--- tests/test_workers/test_tasks/test_fbc_utils.py
+++ tests/test_workers/test_tasks/test_fbc_utils.py
@@ -10,84 +10,72 @@ from iib.workers.tasks.fbc_utils import is_image_fbc
     "skopeo_output,is_fbc",
     [
         (
-                {
-                    "created": "2021-11-10T13:56:39.522635487Z",
-                    "author": "Bazel",
-                    "architecture": "amd64",
-                    "os": "linux",
-                    "config": {
-                        "User": "0",
-                        "Env": [
-                            "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/busybox",
-                            "SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt"
-                        ],
-                        "Entrypoint": [
-                            "/bin/opm"
-                        ],
-                        "Cmd": [
-                            "serve",
-                            "/configs"
-                        ],
-                        "WorkingDir": "/",
-                        "Labels": {
-                            "io.buildah.version": "1.23.1",
-                        }
-                    },
-                    "rootfs": {
-                        "type": "layers",
-                        "diff_ids": [
-                            "sha256:c0d270ab7e0db0fa1db41d15b679a7b77ffbb9db62790095c7aee41444435933",
-                        ]
-                    },
-                    "history": [
-                        {
-                            "created": "1970-01-01T00:00:00Z",
-                            "created_by": "bazel build ...",
-                            "author": "Bazel"
-                        },
-                    ]
+            {
+                "created": "2021-11-10T13:56:39.522635487Z",
+                "author": "Bazel",
+                "architecture": "amd64",
+                "os": "linux",
+                "config": {
+                    "User": "0",
+                    "Env": [
+                        "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+                        "SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt",
+                    ],
+                    "Entrypoint": ["/bin/opm"],
+                    "Cmd": ["serve", "/configs"],
+                    "WorkingDir": "/",
+                    "Labels": {"io.buildah.version": "1.23.1"},
                 },
-                False
+                "rootfs": {
+                    "type": "layers",
+                    "diff_ids": [
+                        "sha256:c0d270ab7e0db0fa1db41d15b679a7b77ffbb9db62790095c7aee41444435933",
+                    ],
+                },
+                "history": [
+                    {
+                        "created": "1970-01-01T00:00:00Z",
+                        "created_by": "bazel build ...",
+                        "author": "Bazel",
+                    },
+                ],
+            },
+            False,
         ),
         (
-                {
-                    "created": "2021-11-10T13:56:39.522635487Z",
-                    "author": "Bazel",
-                    "architecture": "amd64",
-                    "os": "linux",
-                    "config": {
-                        "User": "0",
-                        "Env": [
-                            "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/busybox",
-                            "SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt"
-                        ],
-                        "Entrypoint": [
-                            "/bin/opm"
-                        ],
-                        "Cmd": [
-                            "serve",
-                            "/configs"
-                        ],
-                        "WorkingDir": "/",
-                        "Labels": {
-                            "io.buildah.version": "1.23.1",
-                            "operators.operatorframework.io.index.configs.v1": "/configs"
-                        }
+            {
+                "created": "2021-11-10T13:56:39.522635487Z",
+                "author": "Bazel",
+                "architecture": "amd64",
+                "os": "linux",
+                "config": {
+                    "User": "0",
+                    "Env": [
+                        "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+                        "SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt",
+                    ],
+                    "Entrypoint": ["/bin/opm"],
+                    "Cmd": ["serve", "/configs"],
+                    "WorkingDir": "/",
+                    "Labels": {
+                        "io.buildah.version": "1.23.1",
+                        "operators.operatorframework.io.index.configs.v1": "/configs",
                     },
-                    "rootfs": {
-                        "type": "layers",
-                        "diff_ids": [
-                            "sha256:c0d270ab7e0db0fa1db41d15b679a7b77ffbb9db62790095c7aee41444435933",
-                        ]
-                    },
-                    "history": [
-                        {
-                            "created": "1970-01-01T00:00:00Z",
-                            "created_by": "bazel build ...",
-                            "author": "Bazel"
-                        },
-                    ]
                 },
+                "rootfs": {
+                    "type": "layers",
+                    "diff_ids": [
+                        "sha256:c0d270ab7e0db0fa1db41d15b679a7b77ffbb9db62790095c7aee41444435933",
+                    ],
+                },
+                "history": [
+                    {
+                        "created": "1970-01-01T00:00:00Z",
+                        "created_by": "bazel build ...",
+                        "author": "Bazel",
+                    },
+                ],
+            },
             True,
         ),
     ],
