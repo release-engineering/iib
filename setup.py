a723a53af26b3ffb77badd102220f679bd7372c6
Zainab Alsaffar <zalsaffa@redhat.com>
2022-03-16 17:55:22 -0400

    IIB Release v6.2.0 (#396)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iib',
-    version='6.1.0',
+    version='6.2.0',
     long_description=__doc__,
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
