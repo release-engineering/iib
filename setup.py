a5a385c6bd6b8749d074de61b66afda17a8c3baa
Jan Lipovský <jlipovsk@redhat.com>
2022-09-15 16:25:01 +0200

    IIB 6.6.1 release
    - Add recursive-related-bundles endpoint
    - Adding ability to create single-active-consumer queues
    - Add static types and mypy checks
    - Append overwrite index token to current docker config
    - Fixed config loading

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iib',
-    version='6.6.0',
+    version='6.6.1',
     long_description=__doc__,
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
