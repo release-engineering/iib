3aaa11910ee6ddc51b3fedab8fcef852b7207ffb
Yashvardhan Nanavati <yashn@bu.edu>
2022-10-26 18:59:36 -0400

    Release IIB v6.7.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iib',
-    version='6.7.1',
+    version='6.7.2',
     long_description=__doc__,
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
