02680d3559ffbaf0076ed69c906b231448d70361
Yashvardhan Nanavati <yashn@bu.edu>
2023-02-14 10:30:15 -0500

    Release IIB v7.0.1
    Refers to CLOUDDST-17259

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iib',
-    version='7.0.0',
+    version='7.0.1',
     long_description=__doc__,
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
