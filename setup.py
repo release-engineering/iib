86b928f9adadc835196ae0bf091e0dc516b54b25
Yashvardhan Nanavati <yashn@bu.edu>
2023-08-28 11:36:28 -0400

    Release IIB v8.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iib',
-    version='8.3.0',
+    version='8.4.0',
     long_description=__doc__,
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
