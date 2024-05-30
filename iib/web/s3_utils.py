c82bb8f80c444a5bab9d61660119248818d3976c
Yashvardhan Nanavati <yashn@bu.edu>
2024-05-30 10:38:43 -0700

    Release s3 resources in a timely manner
    This commit ensures the entire log file is not loaded in-memory
    and is directly streamed from the s3 bucket
    
    Signed-off-by: Yashvardhan Nanavati <yashn@bu.edu>

--- iib/web/s3_utils.py
+++ iib/web/s3_utils.py
@@ -24,10 +24,12 @@ def get_object_from_s3_bucket(
     """
     file_name = f'{s3_key_prefix}/{s3_file_name}'
     log.info('getting file from s3 : %s', file_name)
-    s3 = boto3.resource(service_name='s3')
     try:
-        response = s3.meta.client.get_object(Bucket=bucket_name, Key=file_name)
+        s3_client = boto3.client('s3')
+        response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+        return response['Body'].read()
     except Exception as error:
         log.exception('Unable to fetch object %s from bucket %s: %s', file_name, bucket_name, error)
         return None
-    return response['Body']
+    finally:
+        s3_client.close()
