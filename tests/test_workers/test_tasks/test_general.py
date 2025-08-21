e8ac45130bcede99244435f4d26e90a9887a2d5f
Jonathan Gangi <jgangi@redhat.com>
2025-08-21 14:31:15 -0300

    Fix bug on failed_request_callback
    This commit fixes a bug on `failed_request_callback` which were causing
    the `IIBError` messages to being overwritten with `An unknown error
    occurred. See logs for details`.
    
    It also fixes the unit-test that wasn't properly constructed and didn't
    catch this issue.
    
    Signed-off-by: Jonathan Gangi <jgangi@redhat.com>

--- tests/test_workers/test_tasks/test_general.py
+++ tests/test_workers/test_tasks/test_general.py
@@ -11,7 +11,10 @@ from iib.workers.tasks import general
     'exc, expected_msg',
     (
         (IIBError('Is it lunch time yet?'), 'Is it lunch time yet?'),
-        (RuntimeError('I cannot run in the rain!'), 'An unknown error occurred'),
+        (
+            RuntimeError('I cannot run in the rain!'),
+            'An unknown error occurred. See logs for details',
+        ),
         (FinalStateOverwriteError("can not overwite final state"), "Already in final state"),
     ),
 )
@@ -19,5 +22,8 @@ from iib.workers.tasks import general
 @mock.patch('iib.workers.tasks.general.set_request_state')
 def test_failed_request_callback(mock_srs, mock_cleanup, exc, expected_msg):
     general.failed_request_callback(None, exc, None, 3)
-    mock_srs(3, expected_msg)
+    if isinstance(exc, FinalStateOverwriteError):
+        mock_srs.assert_not_called()
+    else:
+        mock_srs.assert_called_once_with(3, 'failed', expected_msg)
     mock_cleanup.assert_called_once()
